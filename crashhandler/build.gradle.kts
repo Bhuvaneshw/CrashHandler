c206c1d13fc1896091b432e232f7b73fc3e37f7a
Bhuvaneshwaran <bhuvaneshwaran319@gmail.com>
2024-09-04 14:37:11 +0530

    Jitpack config

--- crashhandler/build.gradle.kts
+++ crashhandler/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -29,5 +30,17 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.acutecoder"
+                artifactId = "crashhandler"
+            }
+        }
     }
 }
